686479c7d5c694eab5d4902acf4762c0409f1ecf
Rozzini <Rozzinimr@gmail.com>
2022-10-30 07:07:04 +0400

    1

---
+++ hak1/pages/api/hello.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ cardid: 'qwertqe', ppn: '123455', emirates_id: '228-1337', uid: '1488', 
+                         name: 'John Doe',  alhosn_status: 0, login: 'mraspors',
+                         status: 0, close_reason: '123', date: '997' })
+}
---
+++ hak1/pages/index.js
@@ -0,0 +1,11 @@
+import TopNav from "../components/TopNav";
+import Hero from "../components/Hero";
+
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-br from-[#000000] via-[#423D3D] to-[#777373] h-screen">
+      <TopNav />
+      <Hero />
+    </div>
+  )
+}
